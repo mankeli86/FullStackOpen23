5d4b0f884b054a3fda7f4e0494485e05bee412e7
Tuomas <tuomas.2.makinen@tuni.fi>
2023-04-05 17:53:28 +0300

    Teht 1.1-1.3

---
+++ osa1/kurssitiedot/src/App.js
@@ -0,0 +1,57 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1.name} exercises1={part1.exercises}/>
+      <Content part2={part2.name} exercises2={part2.exercises}/>
+      <Content part3={part3.name} exercises3={part3.exercises}/>
+      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]}/>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return [
+    <h1>{props.course}</h1>
+  ]
+}
+
+const Content = (props) => {
+  return [
+    <Part part={props.part1} exercises={props.exercises1}/>,
+    <Part part={props.part2} exercises={props.exercises2}/>,
+    <Part part={props.part3} exercises={props.exercises3}/> 
+  ]
+}
+
+const Total = (props) => {
+  return [
+    <p>Number of exercises {props.exercises.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    )}
+    </p>
+  ]
+}
+
+const Part = (props) => {
+  return [
+    <p>{props.part} {props.exercises}</p>
+  ]
+}
+
+export default App
