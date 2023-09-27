64a6947d818b3be165349ecd78f300be365891f5
Tuomas <tuomas.2.makinen@tuni.fi>
2023-09-27 18:12:39 +0300

    puhelinluettelo step6

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Filter = ({ search, handleSearch }) => {
   return (
@@ -31,17 +32,21 @@ const Persons = ({ persons }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameSearch, setNameSearch] = useState('')
   const [filteredNames, setFilteredNames] = useState(persons)
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data)
+        setFilteredNames(response.data)
+    })
+  }, [])
+  
   const addToPhonebook = (event) => {
     event.preventDefault()
     const nameObject = {
