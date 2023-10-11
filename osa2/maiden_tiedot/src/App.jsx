e679f0159b12187f26d39326df7e91723e90746a
Tuomas <tuomas.2.makinen@tuni.fi>
2023-10-12 01:54:59 +0300

    maiden tiedot step1

---
+++ osa2/maiden_tiedot/src/App.jsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
+
+const Filter = ({ search, handleSearch }) => {
+  return (
+    <div>
+      find countries<input value={search} onChange={handleSearch}/>
+    </div>
+  )
+}
+
+const Country = ({ country }) => {
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <div>capital {country.capital}</div>
+      <div>area {country.area}</div>
+      <h3>languages:</h3>
+      <ul>
+        {Object.values(country.languages).map((lang, index) => <li key={index}>{lang}</li>)}
+      </ul>
+      <img src={country.flags.png} />
+    </div>
+  )
+}
+
+const Countries = ({ countries, countryDetail, searchNotEmpty }) => {
+  if (countries.length <= 10 && countries.length > 1) {
+    return (
+      countries.map(country => <div key={country.cca2}>{country.name.common}</div>)
+    )
+  }
+  if (countries.length > 10) {
+    if (searchNotEmpty) {
+      return (
+        <div>Too many matches, specify another filter</div>
+      )
+    }
+  }
+  if (countryDetail) {
+    return (
+      <Country country={countryDetail}/>
+    )
+  }
+}
+
+function App() {
+  const [countrySearch, setCountrySearch] = useState('')
+  const [country, setCountry] = useState(null)
+  const [countryDetail, setCountryDetail] = useState([])
+  const [countries, setCountries] = useState([])
+  const [filteredCountries, setFilteredCountries] = useState([])
+  const allCountriesUrl = "https://studies.cs.helsinki.fi/restcountries/api/all"
+  const countryUrl = "https://studies.cs.helsinki.fi/restcountries/api/name"
+
+  useEffect(() => {
+    axios
+      .get(allCountriesUrl)
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (country) {
+      axios
+        .get(`${countryUrl}/${country.name.common}`)
+        .then(response => {
+          setCountryDetail(response.data)
+        })
+    }
+  }, [country])
+  
+  const handleSearchChange = (event) => {
+    setCountrySearch(event.target.value.toLowerCase())
+    var updatedCountries = [...countries]
+    updatedCountries = countries.filter(p => p.name.common.toLowerCase().includes(event.target.value.toLowerCase()))
+    if (updatedCountries.length === 1) {
+      setCountry(updatedCountries[0])
+    }
+    else {
+      setCountry(null)
+    }
+    setFilteredCountries(updatedCountries)
+  }
+
+  const searchNotEmpty = countrySearch.trim() !== '';
+
+  return (
+    <div>
+      <Filter search={countrySearch} handleSearch={handleSearchChange}/>,
+      <Countries countries={filteredCountries} countryDetail={country} searchNotEmpty={searchNotEmpty}/>
+    </div>
+  )
+}
+
+export default App
