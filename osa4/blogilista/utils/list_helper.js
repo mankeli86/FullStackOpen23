65fdf6231e43341b9d30b5683c6a088e7307198f
Tuomas <tuomas.2.makinen@tuni.fi>
2023-12-16 20:05:01 +0200

    4.5.

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -10,7 +10,25 @@ const totalLikes = (blogs) => {
   }
 }
 
+const favoriteBlog = (blogs) => {
+  if (Array.isArray(blogs) && blogs.length) {
+    const sorted = blogs.sort((a, b) => {
+      return b.likes - a.likes
+    })
+    const favorite = sorted[0]
+    return {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes
+    }
+  } else {
+    return {}
+  }
+
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
