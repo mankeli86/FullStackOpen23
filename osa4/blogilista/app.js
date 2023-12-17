cf3b21ecbe65ae8a7472f57d29d1ed5ad7357eed
Tuomas <tuomas.2.makinen@tuni.fi>
2023-12-17 17:17:27 +0200

    4.10.-4.12.

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,10 +1,12 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 
@@ -22,5 +24,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.errorHandler)
 
 module.exports = app
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,13 +1,32 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+]
 
 test('correct amount of blogs is returned', async () => {
   const res = await api.get('/api/blogs')
 
-  expect(res.body).toHaveLength(6)
+  expect(res.body).toHaveLength(2)
 })
 
 test('id field is defined in every blog', async () => {
@@ -16,6 +35,89 @@ test('id field is defined in every blog', async () => {
   res.body.forEach(blog => {
     expect(blog.id).toBeDefined()  
   })
+})
+
+test('new blog can be added', async () => {
+  const newBlog = {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const res = await api.get('/api/blogs')
+  expect(res.body).toHaveLength(3)
+})
+
+test('adding a new blog without likes field sets likes to zero', async () => {
+  const newBlog = {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    __v: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const res = await api.get('/api/blogs')
+  expect(res.body.map(blog => blog.likes)).toContain(0)
+})
+
+test('blog without title is not added', async () => {
+  const newBlog = {
+    _id: "5a422bc61b54a676234d17fc",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    __v: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const res = await api.get('/api/blogs')
+
+  expect(res.body).toHaveLength(initialBlogs.length)
+})
+
+test('blog without url is not added', async () => {
+  const newBlog = {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    __v: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const res = await api.get('/api/blogs')
+
+  expect(res.body).toHaveLength(initialBlogs.length)
+})
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
 })
 
 afterAll(async () => {
