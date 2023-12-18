5fb4acbfdd1314248e177acf00ab400e180c5816
Tuomas <tuomas.2.makinen@tuni.fi>
2023-12-18 19:09:56 +0200

    4.21.

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -31,8 +31,18 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+  const blog = await Blog.findById(request.params.id)
+  if ( user._id.toString() === blog.user.toString() ) {
+    await blog.deleteOne()
+    response.status(204).end()
+  } else {
+    return response.status(403).json({ error: 'no permission to delete this blog' })
+  }
 })
 
 blogsRouter.put('/:id', async (request, response) => {
