1d2c94e9793717ea2fe65e497b19a89e87f4e478
Tuomas <tuomas.2.makinen@tuni.fi>
2023-12-20 00:12:56 +0200

    5.3.-5.4.

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -9,7 +9,11 @@ const App = () => {
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
+  const [notificationMsg, setNotificationMsg] = useState(null)
+  const [notificationType, setNotificationType] = useState(null)
+  const [blogTitle, setBlogTitle] = useState('')
+  const [blogAuthor, setBlogAuthor] = useState('')
+  const [blogUrl, setBlogUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -34,6 +38,47 @@ const App = () => {
     setPassword(event.target.value)
   }
 
+  const handleBlogTitleChange = (event) => {
+    setBlogTitle(event.target.value)
+  }
+
+  const handleBlogAuthorChange = (event) => {
+    setBlogAuthor(event.target.value)
+  }
+
+  const handleBlogUrlChange = (event) => {
+    setBlogUrl(event.target.value)
+  }
+
+  const addBlog = async (event) => {
+    event.preventDefault()
+    try {
+      const newBlog = {
+        title: blogTitle,
+        author: blogAuthor,
+        url: blogUrl
+      }
+      const response = await blogService.create(newBlog)
+      setBlogs(blogs.concat(response))
+      setBlogTitle('')
+      setBlogAuthor('')
+      setBlogUrl('')
+      setNotificationMsg(`a new blog ${newBlog.title} by ${newBlog.author} added`)
+      setNotificationType('add')
+      setTimeout(() => {
+        setNotificationMsg(null)
+        setNotificationType(null)
+      }, 4000)
+    } catch (e) {
+      setNotificationMsg('adding new blog failed')
+      setNotificationType('error')
+      setTimeout(() => {
+        setNotificationMsg(null)
+        setNotificationType(null)
+      }, 4000)
+    }
+  }
+  
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
@@ -49,10 +94,12 @@ const App = () => {
       setUsername('')
       setPassword('')
     } catch (e) {
-      setErrorMessage('wrong credentials')
+      setNotificationMsg('wrong username or password')
+      setNotificationType('error')
       setTimeout(() => {
-        setErrorMessage(null)
-      }, 3000)
+        setNotificationMsg(null)
+        setNotificationType(null)
+      }, 4000)
     }
   }
 
@@ -61,12 +108,25 @@ const App = () => {
     window.localStorage.clear()
     setUser(null)
   }
+
+  const blogForm = () => {
+    return (
+      <form onSubmit={addBlog}>
+        title:<input value={blogTitle} onChange={handleBlogTitleChange}/>
+        <br/>
+        author:<input value={blogAuthor} onChange={handleBlogAuthorChange}/>
+        <br/>
+        url:<input value={blogUrl} onChange={handleBlogUrlChange}/>
+        <button type='submit'>create</button>
+      </form>
+    )
+  }
   
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
-        <Notification message={errorMessage} />
+        <Notification message={notificationMsg} type={notificationType}/>
         <form onSubmit={handleLogin}>
           username<input value={username} onChange={handleUsernameChange}/>
           <br/>
@@ -81,8 +141,11 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <Notification message={notificationMsg} type={notificationType}/>
       <div>{user.name} logged in<button onClick={(e) => handleLogout(e)}>logout</button></div>
       <br/>
+      <h2>create new</h2>
+      {blogForm()}
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
